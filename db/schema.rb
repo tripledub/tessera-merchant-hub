bc4fa79e3630b9a8eef1088d8863f577c73c5151
Stewart Campbell <stewartcuk@gmail.com>
2026-06-19 08:59:57 +0100

    feat: add address_match_method and address_match_confidence to kyc_documents [MH-87]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_18_162155) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_19_075836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -43,6 +43,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_18_162155) do
   end
 
   create_table "kyc_documents", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.decimal "address_match_confidence", precision: 4, scale: 3
+    t.string "address_match_method"
     t.uuid "applicant_id", null: false
     t.datetime "created_at", null: false
     t.uuid "kyc_principal_id"
