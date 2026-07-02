b8ea2f45e69c3cedb7de4df2b5568ef619979384
Stewart Campbell <stewartcuk@gmail.com>
2026-07-02 09:01:58 +0100

    MH-178 Update certificate schemas to extract structured business address fields

--- app/models/extraction_data/certificate_of_incorporation.rb
+++ app/models/extraction_data/certificate_of_incorporation.rb
@@ -2,14 +2,23 @@
 
 module ExtractionData
   class CertificateOfIncorporation < Base
+    include Concerns::BusinessAddressProviding
+
     register_as :certificate_of_incorporation
 
     attribute :company_name, :string
     attribute :registration_number, :string
     attribute :date_of_incorporation, :date
     attribute :jurisdiction, :string
-    attribute :registered_address, :string
+    attribute :line1, :string
+    attribute :city, :string
+    attribute :postcode, :string
+    attribute :country, :string
 
     validates :company_name, :registration_number, presence: true
+
+    def structured_address
+      { line1: line1, city: city, postcode: postcode, country: country }
+    end
   end
 end
